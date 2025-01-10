dc82afedaf93ef3c452eab203f71e5034c644b72
TonyStark <sarkarsubhodeep417@gmail.com>
2025-01-10 14:45:47 +0530

    nonprimitive dded

---
+++ primitive/Primitives.js
@@ -0,0 +1,49 @@
+/*
+1.Number
+2.String
+3.boolean
+4.Undefiend
+5.null.
+6.symbol
+
+*/
+
+// Numbers
+let Bonus = 100;
+let anotherBous = new Number(120);
+
+// console.log(Bonus);
+// console.log(typeof Bonus);
+// console.log(anotherBous.valueOf());
+// console.log(typeof anotherBous);
+
+// string
+let myString = "subhodeep";
+let userName = "Subhodeep2005s";
+let lastName = `sarkar`;
+
+let oldGreet = "Hello" + " " + myString;
+let NewGreet = `Hello ${myString}!`;
+let Demo=`The Value Is ${2 * 2}`
+// console.log(Demo);
+// console.log(NewGreet);
+
+// console.log(userName);
+
+// Boolean
+let isActive = true;
+
+// undefiend & null
+let firstname;
+
+let lastname = null;
+
+// console.log(firstname);
+// console.log(lastname);
+
+
+// symbol
+
+let sm1=Symbol("subhodeep");
+let sm2=Symbol("sarkar");
+console.log(sm1)
